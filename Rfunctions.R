6ed6190247ddb3d57586d09d4f2f37c5226b5e85
ykimmate14 <ykimmate14@users.noreply.github.com>
2016-02-08 22:19:12 -0600

    Update Rfunctions.R

--- Rfunctions.R
+++ Rfunctions.R
@@ -1,4 +1,5 @@
-# this function takes a dataset as an argument and generates a data frame whose first column shows the dataset's variables and second column shows the class of each variable
+# this function takes a dataset as an argument and generates a data frame whose first column 
+# shows the dataset's variables and second column shows the class of each variable
 
 whatClass <- function(data){
   varClass <- character()
@@ -11,7 +12,8 @@ whatClass <- function(data){
 }
 
 
-# this function takes a dataset as an argument and generates a data frame whose first column shows the dataset's variables and second column shows the number of NA of each variable
+# this function takes a dataset as an argument and generates a data frame whose first column shows the dataset's 
+# variables and second column shows the number of NA of each variable
 
 NAnum <- function(data){
   y <- integer()
@@ -23,3 +25,14 @@ NAnum <- function(data){
   return (result)
 }
 
+# the function takes data frame and column number as arguments and return data frame with 
+# unique values on the specified column and its number of occurrence.
+NofRow <- function(data, c){
+    list1 <- unique(data[,c])
+    list2 <- list()
+    for(i in 1:length(list1)){
+        list2 <- c(list2, sum(data[,2] %in% list1[i]))
+    }
+    result <- data.frame(uniqueV = list1, count = unlist(list2))
+    return(result)
+}
